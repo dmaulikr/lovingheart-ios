16e114fe7adc2957ed009eef2b3b90ce4bc845f3
Edward Chiang <edwardubuntu@gmail.com>
2014-03-26 14:24:08 +0800

    Setup setting page

---
+++ LovingHeart/LHSettingsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  LHSettingsTableViewController.h
+//  LovingHeart
+//
+//  Created by Edward Chiang on 2014/3/26.
+//  Copyright (c) 2014年 LovineHeart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHSettingsTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButtonItem;
+
+@end
